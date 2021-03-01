b8a4540ac900399dbd754e229533c8ce10841d66
jdm-git <58112825+jdm-git@users.noreply.github.com>
2021-03-01 20:44:07 +0100

    entry loging logic

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const users = require('./routes/users');
+const auth = require('./routes/auth');
 const app = express();
 
 mongoose.connect('mongodb://localhost/vidly', {useNewUrlParser: true, useUnifiedTopology: true} )
@@ -9,6 +10,7 @@ mongoose.connect('mongodb://localhost/vidly', {useNewUrlParser: true, useUnified
 
 app.use(express.json());
 app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
 const {User, validate} = require('../models/user');
-const mongoose = require('mongoose');
+const _ = require('lodash');
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+
 
 
 
@@ -12,14 +14,16 @@ router.post('/', async(req, res) => {
     let user = await User.findOne({ email: req.body.email});
     if(user) return res.status(400).send('User already registered.');
 
-    user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    });
-    await user.save();
+    user = new User(_.pick(req.body, ['name', 'email', 'password']));
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
 
-    res.send(user);
+    await user.save();
+    console.log('Elo nowy user');
+    const pick = _.pick(user, [
+        'name', 'email'
+    ]);
+    res.send(pick);
 
 });
 module.exports = router;
