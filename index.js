fdca62af7a1823b705a19e4af6c2d41fbe0dd170
jdm-git <58112825+jdm-git@users.noreply.github.com>
2021-03-01 22:05:59 +0100

    json web token

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const mongoose = require('mongoose');
 const users = require('./routes/users');
 const auth = require('./routes/auth');
 const app = express();
+const config = require('config');
+
+if(!config.get('jwtPrivateKey')){
+    console.error('FATAL ERROR: jwtPrivateKey is not defined.');
+    process.exit(1);
+}
 
 mongoose.connect('mongodb://localhost/vidly', {useNewUrlParser: true, useUnifiedTopology: true} )
   .then(() => console.log('Connected to MongoDB...'))
--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,8 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const Joi = require('joi');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 
 router.post('/', async(req, res) => {
@@ -15,8 +17,9 @@ router.post('/', async(req, res) => {
 
     const validPassword = await bcrypt.compare(req.body.password, user.password);
     if(!validPassword) return res.status(400).send('Invalid email or password');
-    console.log(user);
-    res.send(true);
+
+    const token = jwt.sign({ _id: user._id }, config.get('jwtPrivateKey'));
+    res.send(token);
 });
 
 function validate(req){
