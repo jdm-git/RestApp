491bd34fb0299419cdd9c13482d5e4ae40e2ff1f
jdm-git <58112825+jdm-git@users.noreply.github.com>
2021-03-01 20:04:16 +0100

    registering user

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const users = require('./routes/users');
 const app = express();
 
 mongoose.connect('mongodb://localhost/vidly', {useNewUrlParser: true, useUnifiedTopology: true} )
@@ -7,6 +8,7 @@ mongoose.connect('mongodb://localhost/vidly', {useNewUrlParser: true, useUnified
   .catch(err => console.error('Could not connect to MongoDB...'));
 
 app.use(express.json());
+app.use('/api/users', users);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- models/user.js
+++ models/user.js
@@ -24,13 +24,13 @@ const User = mongoose.model('User', new mongoose.Schema({
 }));
 
 function validateUser(user){
-    const schema = {
+    const schema = Joi.object({
         name: Joi.string().min(5).max(50).required(),
         email: Joi.string().min(5).max(255).required().email(),
         password: Joi.string().min(5).max(255).required()
-    };
+    });
 
-    return schema.validate(user)
+    return schema.validate(user);
 }
 
 exports.User = User;
